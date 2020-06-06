a1af05362e4e02b189cd870c0956ef3b0b3d4bba
Tito <hduartea1@miumg.edu.gt>
2020-06-06 00:26:55 -0600

    Se agregaron las funciones

--- Aplicacion.py
+++ Aplicacion.py
@@ -47,4 +47,72 @@ class Desk:
         Button(frame, text = 'funcion4', command = self.funcion4).grid(row = 6, column = 3 , sticky = W + E)
         Button(frame, text = 'funcion5', command = self.funcion5).grid(row = 6, column = 4 , sticky = W + E)
  
+ self.message = Label(text = '', fg = 'red')
+        self.message.grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+
+    
+    def funcion1(self):
+        dia=int(self.var3.get())
+        mes=int(self.var4.get())
+        anio=int(self.var5.get())
+        bindia=format(dia, '0b' )
+        binmes=format(mes, '0b')
+        binanio=format(anio, '0b')
+
+        self.message['text'] = 'La fecha es: {}/{}/{} y  en binario es:{}/{}/{}'.format(dia,mes,anio,bindia,binmes,binanio)
+
+
+    def funcion2(self):
+            
+        dia=int(self.var3.get())
+        mes=int(self.var4.get())
+        anio=int(self.var5.get())
+        fecha_de_nacimiento = datetime.datetime(anio, mes, dia)
+        fecha_de_hoy = datetime.datetime.now()
+        diferencia = fecha_de_hoy - fecha_de_nacimiento
+        dias_vividos = diferencia.days
+        self.message['text'] = 'Usted nacion {}/{}/{}: y a vivido {} dias'.format(dia,mes,anio,dias_vividos)
+
+    def funcion3(self):
+        nombre=str(self.var1.get())
+        apellido=str(self.var2.get())
+        numero_nombre=int(len(nombre))
+        numero_apellido=int(len(apellido))
+        if numero_nombre%2==0 and numero_apellido %2==0 :
+            self.message['text'] = '{} {} su nombre es par y su apellido es par'.format(nombre,apellido)
+        elif numero_nombre%2==0 and numero_apellido %2==1:
+            self.message['text'] = '{} {} su nombre es par y tu apellido es impar'.format(nombre,apellido)
+        elif numero_nombre%2==1 and numero_apellido %2==0:
+            self.message['text'] = '{} {} su nombre es impar y tu apellido es par'.format(nombre,apellido)
+        else:
+            self.message['text'] = '{} {} su nombre es impar y tu apellido es impar'.format(nombre,apellido)
+
+    def funcion4(self):
+        nombre=str(self.var1.get())
+        apellido=str(self.var2.get())
+        cuenta = 0
+        for carac in nombre:
+            if carac == 'a' or carac =='A' or carac =='e' or carac =='E' or carac =='i' or carac=='I' or carac=='o' or carac=="O" or carac=="u" or carac=="U":
+                cuenta += 1
+        for carac in apellido:
+            if carac == 'a' or carac =='A' or carac =='e' or carac =='E' or carac =='i' or carac=='I' or carac=='o' or carac=="O" or carac=="u" or carac=="U":
+                cuenta += 1
+        cuntal=len(nombre)
+        cuntal1=len(apellido)
+        consonante=cuntal+cuntal1-cuenta
+
+        self.message['text'] = 'Su nombre y apellido tienen {} vocales y {} consonantes'.format(cuenta,consonante)
+     
+
+    def funcion5(self):
+        nombre=str(self.var1.get())
+        apellido=str(self.var2.get())
+        cadena_invertida = ""
+        cadena_invertida1= ""
+        for letra in nombre:
+            cadena_invertida = letra + cadena_invertida
+        for letra1 in apellido:
+            cadena_invertida1 = letra1 + cadena_invertida1
+        self.message['text'] = '{} {} o al revez {} {}'.format(nombre,apellido,cadena_invertida,cadena_invertida1)
+
 
