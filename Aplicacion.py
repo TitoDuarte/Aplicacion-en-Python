71b95a6a72a335722402c88f6e6cc3a5ba30c0c5
Tito <hduartea1@miumg.edu.gt>
2020-06-06 00:15:19 -0600

    Se coloco el Ancho y Alto a la aplicaicon

--- Aplicacion.py
+++ Aplicacion.py
@@ -2,3 +2,10 @@ from tkinter import ttk
 from tkinter import *
 import math
 import datetime
+class Desk:
+    def __init__(self, window):
+      
+        Ancho = 500
+        Alto = 250
+        
+        
